7c8bb1ecefd0373e5616ac7f925cdb20286456ea
Athul <akkv.2507@gmail.com>
2025-04-01 14:05:45 +0530

    Added Email field in AdminWallet

--- internals/core/models/admin.go
+++ internals/core/models/admin.go
@@ -7,8 +7,9 @@ import (
 )
 
 type AdminWallet struct {
-	ID        uuid.UUID `json:"id" gorm:"type:uuid;primary_key"` 
-	Balance   float64   `json:"balance"`                         
-	CreatedAt time.Time `json:"created_at"`                    
-	UpdatedAt time.Time `json:"updated_at"`                     
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;primary_key"`
+	Email     string    `json:"email"`
+	Balance   float64   `json:"balance"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
